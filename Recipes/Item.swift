b9ab5e9a854ec2a57159c8f741ee8757fcef342b
John Fairhurst <johnfairh@gmail.com>
2025-02-11 12:36:02 +0000

    Initial Commit

---
+++ Recipes/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Recipes
+//
+//  Created by John on 11/02/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
