d511aa8b3ba0f2337bbec485f3b7d9ddb6c2c739
John Fairhurst <johnfairh@gmail.com>
2025-07-01 16:42:08 +0100

    Search history

--- Recipes/Views/HistoryView.swift
+++ Recipes/Views/HistoryView.swift
@@ -5,36 +5,45 @@
 //  Created by John on 30/06/2025.
 //
 
-// Searchable - try overlay NotFound & backport
 // NavigationState adventure - to add history from recipe-view and recipe-view from history cell-click
 
 import SwiftUI
 import SwiftData
 
 struct HistoryView: View {
-    @Environment(\.modelContext) var modelContext
-    @Query(sort: \Cooking.timestamp, order: .reverse) var cookings2: [Cooking]
-
     @SectionedQuery(\Cooking.monthCode, sort: \Cooking.timestamp, order: .reverse)
     var cookings: SectionedResults<Int, Cooking>
+
+    @State var searchText = ""
+
+    var searchedCookings: SectionedResults<Int, Cooking> {
+        if searchText.isEmpty {
+            return cookings
+        }
+        return cookings.filter { $0.recipe.name.localizedCaseInsensitiveContains(searchText) }
+    }
 
     var body: some View {
         NavigationStack {
             List {
-                ForEach(cookings) { section in
-                    Section(section.id.decodeMonthCode) {
-                        ForEach(section) { cooking in
-                            HStack {
-                                Image(systemName: cooking.recipe.symbolName)
-                                    .imageScale(.large)
-                                    .foregroundStyle(Color.accentColor)
-                                    .frame(minWidth: 32, maxWidth: 32)
-                                VStack(alignment: .leading) {
-                                    Text(cooking.recipe.name).font(.title3)
-                                    Text(cooking.timestamp.formatted(date: .complete, time: .omitted)).font(.body)
+                if !searchText.isEmpty && searchedCookings.isEmpty {
+                    ContentUnavailableView.search(text: searchText)
+                } else {
+                    ForEach(searchedCookings) { section in
+                        Section(section.id.decodeMonthCode) {
+                            ForEach(section) { cooking in
+                                HStack {
+                                    Image(systemName: cooking.recipe.symbolName)
+                                        .imageScale(.large)
+                                        .foregroundStyle(Color.accentColor)
+                                        .frame(minWidth: 32, maxWidth: 32)
+                                    VStack(alignment: .leading) {
+                                        Text(cooking.recipe.name).font(.title3)
+                                        Text(cooking.timestamp.formatted(date: .complete, time: .omitted)).font(.body)
+                                    }
+                                    .padding(.leading, 8)
+                                    Spacer()
                                 }
-                                .padding(.leading, 8)
-                                Spacer()
                             }
                         }
                     }
@@ -44,6 +53,7 @@ struct HistoryView: View {
 #if os(macOS)
             .navigationSplitViewColumnWidth(min: 180, ideal: 200)
 #endif
+            .searchable(text: $searchText, placement: .navigationBarDrawer, prompt: "Recipe name")
         }
     }
 }
